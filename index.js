a06d451f0af9992d79328dd9461f597143294e76
musin2 <muhsindaud75@gmail.com>
2024-07-08 19:44:48 +0300

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(act1 = "roller-skate"){
+    return "This Saturday, I want to "+ act1 + "!";
+}
+
+saturdayFun();
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+};
+
+function wrapAdjective(flair = "*"){
+   return function (adjective = "special"){
+    return `You are ${flair}${adjective}${flair}!`;
+   } ;
+}
+const callChain = wrapAdjective("!!!");
+console.log(callChain("a good developer"));
+console.log(wrapAdjective("||")("something"));
